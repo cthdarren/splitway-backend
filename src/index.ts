edbd3ad9f254caa753e84e57ed9ff25eff43b30c
cthdarren <ctze.darren@gmail.com>
2025-01-19 17:57:26 +0100

    figured out json body, going to write the routes now

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import { Group, PrismaClient } from "@prisma/client";
+import { Group, PrismaClient, User } from "@prisma/client";
 import { Request, Response } from "express";
-import 'dotenv/config'
-import { randomUUID } from "crypto";
+import "dotenv/config";
+import { randomBytes, randomUUID } from "crypto";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 const express = require("express");
 
 const app = express();
+
+// this enables the application to read JSON data
+// wasted so much time trying to figure out why
+// it kept returning undefined bruh
+app.use(express.json())
 
 const cors = require("cors");
 let corsOptions = {
@@ -14,46 +19,84 @@ let corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.get("/testdbconnection", async (req :Request, res :Response) => {
-    const group :Group= await prisma.group.create({
-        data: {
-            name: "Test",
-            id: randomUUID(), 
-            category: "Test",
-            currency: "TES",
-            sharelink: "test",
-            userId: "test"
-        }
-    })
-    const test = await prisma.group.findMany()
-    console.log(test)
-    res.send(test)
-})
+// TODO work with passportjs to introduce authentication
+//
+// app.post("/register", async (req: Request, res: Response) => {
+//     const missingFields :string[] = []
+//     if (req.body.username === undefined){
+//         missingFields.push("username")
+//     }
+//
+//     if (req.body.email === undefined){
+//         missingFields.push("email")
+//     }
+//
+//     if (req.body.password === undefined){
+//         missingFields.push("password")
+//     }
+//
+//     if (req.body.verifypassword === undefined){
+//         missingFields.push("verify password")
+//     }
+//
+//     if (missingFields.length > 0){
+//         res.send(missingFields.join(",") + " fields cannot be empty")
+//     }
+//     
+//     try {
+//         const usernameCheck = await prisma.user.findUnique({
+//             where: { username: req.body.username }
+//         });
+//
+//         const emailCheck = await prisma.user.findUnique({
+//             where: { email: req.body.email}
+//         });
+//         if (usernameCheck === null) {
+//             const user: User = await prisma.user.create({
+//                 data: {
+//                     id: randomUUID(),
+//                     username: req.body.username,
+//                     pwhash: ,
+//                     salt: randomBytes(8),
+//                     email: req.body.email,
+//                     locale: "en_us",
+//                     status: "ACTIVE",
+//                     pictureurl: "http://lmao.com"
+//                 }
+//             });
+//             res.send(user);
+//         }
+//         res.send("Username already exists!");
+//     } catch (err){
+//         console.error(err)
+//         res.send("Invalid username");
+//     }
+// });
 
-app.get("/groups/:id", (req :Request, res :Response) => {
-        const id = req.params.id
-        res.send({
-            id: id,
-            name: "Japan Trip" + id,
-            currency: "JPY",
-            expenditure: 500,
-            members: [
-                {
-                    id: 1,
-                    name: "Darren"
-                },
-                {
-                    id: 2,
-                    name: "Jason"
-                },
-                { id: 3, name: "Pin Kang" }
-            ]
-        })
+app.get("/groups/:id", (req: Request, res: Response) => {
+    const id = req.params.id;
+    res.send({
+        id: id,
+        name: "Japan Trip" + id,
+        currency: "JPY",
+        expenditure: 500,
+        members: [
+            {
+                id: 1,
+                name: "Darren"
+            },
+            {
+                id: 2,
+                name: "Jason"
+            },
+            { id: 3, name: "Pin Kang" }
+        ]
+    });
     console.log("HIT!");
 });
 
-app.get("/expenses/:id", (req :Request, res :Response) => {
-    const id = req.params.id
+app.get("/expenses/:id", (req: Request, res: Response) => {
+    const id = req.params.id;
     res.send([
         {
             id: 1,
