d588e9144b5d8988d95e38bed41a7b58dfae9f44
cthdarren <ctze.darren@gmail.com>
2025-01-19 18:31:21 +0100

    did started on create, need to add testing before progressing

--- src/routes/groups/groups.ts
+++ src/routes/groups/groups.ts
@@ -1,19 +1,65 @@
 import { Group, PrismaClient } from "@prisma/client";
+import { randomUUID, UUID } from "crypto";
 import { Router, Request, Response } from "express";
 
 const router = Router();
 
 // TODO: Remove this and figure out how to import the same prismaclient for the whole app in other areas
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
+const generateShareLink = () => {
+    return "INSERTLINKHERE";
+};
 // GET groups that the user is currently in
-router.get('/groups', async (req: Request, res: Response) => {
-    const groupData :Group[] = []
-    const groups = await prisma.groupMember.findMany({select: {group: true}, where: {userId: req.body.userId}})
-    groups.map(group => {
-        groupData.push(group.group)
-    })
-    res.send({success: true, data: groupData})
+router.get("/groups", async (req: Request, res: Response) => {
+    const groupData: Group[] = [];
+    const groups = await prisma.groupMember.findMany({
+        select: { group: true },
+        where: { userId: req.body.userId }
+    });
+    groups.map((group) => {
+        groupData.push(group.group);
+    });
+
+    res.send({ success: true, data: groupData });
+});
+
+router.get("/create", async (req: Request, res: Response) => {
+    const nameFieldEmpty: boolean = req.body.name === undefined;
+    const categoryFieldEmpty: boolean = req.body.name === undefined;
+    const currencyFieldEmpty: boolean = req.body.name === undefined;
+    const userIdFieldEmpty: boolean = req.body.name === undefined;
+
+    let newGroupId: UUID = randomUUID();
+    while (prisma.group.findUnique({ where: { id: newGroupId } }) !== undefined)
+        newGroupId = randomUUID();
+
+    await prisma.$transaction([
+        prisma.group.create({
+            data: {
+                id: newGroupId,
+                name: req.body.name,
+                category: req.body.category,
+                currency: req.body.currency,
+                sharelink: generateShareLink(),
+                createdBy: req.body.createdBy
+            }
+        }),
+        prisma.groupMember.create({
+            data: {
+                userId: req.body.createdBy,
+                groupId: newGroupId
+            }
+        }),
+        req.body.participants.map((participantId: string) => {
+            prisma.groupMember.create({
+                data: {
+                    userId: participantId,
+                    groupId: newGroupId
+                }
+            });
+        })
+    ]);
 });
 
 export default router;
