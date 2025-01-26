e396c23a3f6eeccc8cbebfa53101c797250d6332
cthdarren <ctze.darren@gmail.com>
2025-01-26 14:14:19 +0800

    readded schema and migrations

--- server.py
+++ server.py
@@ -1,13 +1,24 @@
+import asyncio
+from prisma import Prisma
 from typing import Union
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+# async def main() -> None:
+
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    prisma = Prisma()
+    await prisma.connect()
+
+    user = await prisma.group.find_many()
+
+    await prisma.disconnect()
+
+    return user
 
 
 @app.get("/items/{item_id}")
