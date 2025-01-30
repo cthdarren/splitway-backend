f6040fa26dc1c9a08322f653a66bf94f327dbb80
cthdarren <ctze.darren@gmail.com>
2025-01-30 20:27:26 +0800

    changed to sqlalchemy, started on group routes

--- app/api/groups.py
+++ app/api/groups.py
@@ -1,6 +1,14 @@
-from typing import Annotated, Dict
+from typing import Annotated, Any, Dict, Sequence
 
+from sqlalchemy import Row, text
 from fastapi import APIRouter, Header
+from app.sqlalchemyclient import engine
+from pydantic import BaseModel
+
+class CreateGroupItem(BaseModel):
+    name: str
+    category: str
+    currency: str
 
 router = APIRouter(
     prefix="/groups",
@@ -10,14 +18,21 @@ router = APIRouter(
 
 @router.get("/")
 async def getGroups() -> Dict:
-    groupData = {}
+    groupData = []
+    with engine.connect() as c:
+        result = c.execute(text('SELECT id, name, category, currency, sharelink from "Group";'))
+        groupData.extend(result.mappings().all())
+        # result = c.execute(text('SELECT username, email, status, pictureurl from "User"'))
+        # for row in result:
+        #     groupData.append({'id': row.id, 'name': row.name, 'category': row.category, 'currency': row.currency, 'sharelink': row.sharelink})
+
 
     return {'success': True, 'data': groupData}
 
 
 @router.post("/create")
-async def createGroups(authorization: Annotated[str|None, Header()] = None):
+async def createGroups(data: CreateGroupItem, authorization: Annotated[str|None, Header()] = None):
 
-    return {'data': authorization}
+    return {'bearer_token': authorization, 'data' : data}
 
 
