51a130b3711e464ddaf1cdc5366a13423add0f21
cthdarren <ctze.darren@gmail.com>
2025-01-31 18:11:14 +0800

    fixed testing environment

--- test/test_groups.py
+++ test/test_groups.py
@@ -6,11 +6,15 @@ import pytest
 
 client = TestClient(app)
 
-@pytest.mark.anyio
 async def test_group_create():
     await create_fake_user("testing")
     fake_token = "Bearer asdf123109fusdfj"
-    response = client.post("/groups/create", headers={"Authorization": "Bearer asdf123109fusdfj"})
+    response = client.post("/groups/create", headers={"Authorization": "Bearer asdf123109fusdfj"}, json={
+    "name": "cthdarren8",
+    "category": "cthdarren8",
+    "currency": "cthdarren8",
+    "userId": "cthdarren8"
+})
     assert response.status_code == 200
-    # assert json.loads(response.content)["data"] == fake_token
+    assert json.loads(response.content)["bearer_token"] == fake_token
 
